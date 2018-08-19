3ea30bf732fdea69a3c4f518c165a2c30adedd77
Nicolaj Egelund <nicomanden@gmail.com>
2018-08-19 18:05:16 +0200

    Debug

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,11 +1,11 @@
 export function handler(event, context, callback) {
-    console.log(process.env.FIREBASE_SECRET)
     const { identity, user } = context.clientContext || {}
 
     callback(null, {
         statusCode: 200,
         body: JSON.stringify({
             msg: {
+                context: context.clientContext,
                 identity: identity,
                 user: user,
                 firebase: process.env.FIREBASE_SECRET
