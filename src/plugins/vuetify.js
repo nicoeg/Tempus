c8a28233d4c08f13a3ca2d4f0ba71baa1f46e734
Nicolaj Egelund <nicolaj@uptime.dk>
2018-11-13 20:28:56 +0100

    Update components and enable tree-shaking for Vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
-    theme: {
-        primary: '#4DBA87'
-    }
+  iconfont: 'md',
+  theme: {
+    primary: '#4DBA87'
+  }
 })
